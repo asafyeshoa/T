d2e785332696f512c4ec7d7580aeffe6a5069ad9
Asaf Yeshoa <asafa097@gmail.com>
2022-08-29 00:06:31 +0300

    added pagination on moblie

--- src/components/PaginationTable.js
+++ src/components/PaginationTable.js
@@ -168,6 +168,29 @@ export default function CustomPaginationActionsTable(props) {
         ).map((row) => {
           return <CharCard key={row.id} row={row} />;
         })}
+
+<TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={rows?.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+
+
       </>
     );
   }
